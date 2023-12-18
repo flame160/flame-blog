94a075459e855979233f4205df56a69eab09d0c4
bodu <bodu@chinaums.com>
2023-12-18 17:28:46 +0800

    （*）deploy.sh将本地master推送到远程main分支

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ cd docs/.vitepress/dist
 
 # git add -A选项告诉Git将所有文件添加到索引 只想添加新创建和修改的文件（不包括已删除的文件），可以使用git add .命令。
 # git commit -m注释
-git init main
+git init
 git add -A
 git commit -m 'deploy'
 
 # git push -f git@github.com:你的git名/你的git项目名.git master:分支名称（ssh）
 # https到main分支（gitHubPages）
-git push -f  https://github.com/flame160/flame-blog.git main
+git push -f  https://github.com/flame160/flame-blog.git master:main #master:main 表示将本地的 master 分支推送到远程的 main 分支
 
 cd -
